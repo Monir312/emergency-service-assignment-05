e2b8306f42a5856f37fca61a6cacec2871278c5c
Monir Islam <monirislam8426@gmail.com>
2025-08-29 20:23:51 +0600

    upadet all section and completed the javascript add to favourite function

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
+// heart added function 
+
+  const heartButtons = document.querySelectorAll('.single-card .fa-heart');
+  const headerHeart = document.querySelector('.react-heart strong');
+
+  heartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      let count = parseInt(headerHeart.textContent);
+      count += 1;
+      headerHeart.textContent = count;
+    });
+  });
+
+
+
+
+
+// coin added and removed function 
+
+
+
+// copied number added function 
 function copyNumber(number) {
   navigator.clipboard.writeText(number);
   alert("Number copied: " + number);
 }
 
+
+
+//call history function 
 function addHistory(service, number) {
   const list = document.getElementById("callHistory");
   const time = new Date().toLocaleTimeString();
